2c3da431b514db4aff2afee35cb12559f0b1c106
VictoriaParkes <victoria.parkes@live.co.uk>
2025-10-14 09:15:50 +0100

    Add template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import psutil
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -13,7 +13,15 @@ def index():
     usage_message = f"CPU Usage: {cpu_percent}%, Memory Usage: {mem_percent}%"
     if message:
         usage_message += f", {message}"
-    return usage_message
+    return render_template(
+        "index.html",
+        cpu_metric=cpu_percent,
+        mem_metric=mem_percent,
+        message=usage_message
+    )
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(
+        debug=False,
+        host='0.0.0.0'
+    )
